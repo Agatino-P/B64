91989d7839dba118402f0345765db3a49ed2f818
Agatino Pesce <agatino.pesce@gmail.com>
2024-03-11 22:38:25 +0100

    Add project files.

---
+++ B64/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace B64;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("1 encode source.zip, 2 decode base64.txt");
+        ConsoleKeyInfo key =Console.ReadKey();
+        if (key.KeyChar == '1')
+        {
+            Byte[] bytes = File.ReadAllBytes("source.zip");
+            String file = Convert.ToBase64String(bytes);
+            File.WriteAllText("base64.txt", file);
+        }
+        if (key.KeyChar == '2')
+        {
+            string file= File.ReadAllText("base64.txt");
+            Byte[] bytes = Convert.FromBase64String(file);
+            File.WriteAllBytes("source.zip", bytes);
+        }
+    }
+}
