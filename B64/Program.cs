2ea8da1e766d3e32661f57b8a29f0100ef3bd88e
Agatino Pesce <agatino.pesce@gmail.com>
2024-05-27 21:14:40 +0200

    With MockOn

--- B64/Program.cs
+++ B64/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Reflection;
 
 namespace B64;
 
@@ -6,19 +7,74 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("1 encode source.zip, 2 decode base64.txt");
-        ConsoleKeyInfo key =Console.ReadKey();
-        if (key.KeyChar == '1')
+        Console.WriteLine("1 encode, 2 decode");
+        bool encode = Console.ReadKey().KeyChar == '1';
+
+
+        if (encode)
         {
-            Byte[] bytes = File.ReadAllBytes("source.zip");
-            String file = Convert.ToBase64String(bytes);
-            File.WriteAllText("base64.txt", file);
+            EncodeDir();
         }
-        if (key.KeyChar == '2')
+        if (!encode)
         {
-            string file= File.ReadAllText("base64.txt");
-            Byte[] bytes = Convert.FromBase64String(file);
-            File.WriteAllBytes("source.zip", bytes);
+            DecodeDir();
         }
+
+    }
+
+    private static void EncodeDir()
+    {
+        string originalDir = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Original");
+        string encodedDir = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Encoded");
+
+        if (!Path.Exists(encodedDir))
+        {
+            Directory.CreateDirectory(encodedDir);
+        }
+
+        DirectoryInfo dir = new DirectoryInfo(originalDir);
+
+        FileInfo[] fileInfos = dir.GetFiles("*.*");
+        foreach (var fileInfo in fileInfos)
+        {
+
+            string targetFilePath = Path.Combine(encodedDir, $"{fileInfo.Name}.b64");
+            EncodeSingleFile(fileInfo.FullName, targetFilePath);
+        }
+    }
+
+    private static void DecodeDir()
+    {
+        string originalDir = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Original");
+        string encodedDir = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Encoded");
+
+        if (!Path.Exists(originalDir))
+        {
+            Directory.CreateDirectory(originalDir);
+        }
+
+        DirectoryInfo dir = new DirectoryInfo(encodedDir);
+
+        FileInfo[] fileInfos = dir.GetFiles("*.*");
+        foreach (var fileInfo in fileInfos)
+        {
+            string targetFilePath = Path.Combine(originalDir, Path.GetFileNameWithoutExtension(fileInfo.Name));
+            DecodeSingleFile(fileInfo.FullName, targetFilePath);
+        }
+    }
+
+
+    private static void DecodeSingleFile(string b64File, string destFile)
+    {
+        string file = File.ReadAllText(b64File);
+        Byte[] bytes = Convert.FromBase64String(file);
+        File.WriteAllBytes(destFile, bytes);
+    }
+
+    private static void EncodeSingleFile(string sourceFile, string b64File)
+    {
+        Byte[] bytes = File.ReadAllBytes(sourceFile);
+        String file = Convert.ToBase64String(bytes);
+        File.WriteAllText(b64File, file);
     }
 }
